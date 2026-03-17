c7b47fd3edea1bb5124ebd882122465c967935be
Emma Ferkranus <emmaferk@gmail.com>
2026-03-17 14:52:56 -0400

    halfway through lesson, commenting on pulled work based on lesson

--- Assets/Scripts/Mechanics/Save/PlayerSaveData.cs
+++ Assets/Scripts/Mechanics/Save/PlayerSaveData.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-[System.Serializable]
+[System.Serializable] // any class that will store data
 public class PlayerSaveData
 {
-    // When using JsonUtility, we must use SIMPLE data types
-    // public Vector3 position;    // (float, float, float)
+    // When using JsonUtility, we must use SIMPLE data types (types in c# and not unity specific, so no vector3)
+    // (float, float, float = x, y, z)
     public float[] position = new float[3];
 }
--- Assets/Scripts/Mechanics/Save/SaveManager.cs
+++ Assets/Scripts/Mechanics/Save/SaveManager.cs
@@ -7,7 +7,7 @@ public class SaveManager : MonoBehaviour
 
     private void Awake()
     {
-        savePath = Application.persistentDataPath + "/player_save.json";
+        savePath = Application.persistentDataPath + "/player_save.json"; // unity given save
     }
 
     // Save Game
@@ -19,8 +19,8 @@ public class SaveManager : MonoBehaviour
         data.position[1] = playerPos.y;
         data.position[2] = playerPos.z;
 
-        // 1. Save our data in a JSON format
-        string json = JsonUtility.ToJson(data, true); // Optional 2nd argument. True makes the file readable
+        // 1. Save our data in a JSON format, value saves to string variable
+        string json = JsonUtility.ToJson(data, true); // Optional 2nd argument for pretty print. True makes the file readable to humans
         // 2. Write to a JSON file
         File.WriteAllText(savePath, json);
         Debug.Log("Game Saved to: " + savePath);
@@ -31,15 +31,15 @@ public class SaveManager : MonoBehaviour
     {
         if(File.Exists(savePath))
         {
-            // File exists!
+            // File exists! (gotta exist to do anything. duh)
             string json = File.ReadAllText(savePath);   // Load the JSON string from savePath
 
-            PlayerSaveData data = JsonUtility.FromJson<PlayerSaveData>(json); // Convert the JSON string into a PlayerSaveData
-            Vector3 playerData = new Vector3(data.position[0], data.position[1], data.position[2]);
-            return playerData;
+            PlayerSaveData data = JsonUtility.FromJson<PlayerSaveData>(json); // Convert the JSON string into a PlayerSaveData variable
+            Vector3 playerData = new Vector3(data.position[0], data.position[1], data.position[2]); // getting data of xyz saved previously into list
+            return playerData; // give that back
         }
 
-        Debug.LogWarning("No save file found at " + savePath);
-        return Vector3.zero;
+        Debug.LogWarning("No save file found at " + savePath); // specifies where the save file wasn't found
+        return Vector3.zero; // all paths must return a value, if no file then no position
     }
 }
